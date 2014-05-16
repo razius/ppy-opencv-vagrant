510ded6c4fde315b90ed2da273aa2dff2ef169af
Silviu Tantos <me@razius.com>
2014-05-16 16:22:07 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,22 @@
+About
+=====
+
+A Vagrant virtual machine that installs all the needed software for the
+`Practical Python and OpenCV <https://www.pyimagesearch.com/practical-python-opencv/>`_
+book.
+
+Requirements
+============
+
+* VirtualBox 4.3.10
+* Vagrant 1.6.2
+
+Getting started
+===============
+
+To initialize the virtual machine run the following command:
+
+    $ vagrant up
+
+The first time the virtual machine is started it will take some time to
+install all the packages so be patient.
---
+++ bootstrap.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+PPA_REPOSITORIES=(
+    'ppa:jon-severinsson/ffmpeg'
+)
+
+PKG_DEPENDENCIES=(
+    'python-setuptools'
+    'python-dev'
+    'build-essential'
+    'git'
+    'g++'
+    'gfortran'
+    'libopenblas-dev'
+    'liblapack-dev'
+)
+
+# Enable multiverse.
+sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list
+
+apt-get update
+apt-get install -y ${PKG_DEPENDENCIES[@]}
+
+# Fix for matplotlib bug #3029.
+# See: https://github.com/matplotlib/matplotlib/issues/3029
+if [ ! -f "/usr/include/freetype2/ft2build.h" ]; then
+    ln -s /usr/include/freetype2/ft2build.h /usr/include/
+fi
+
+# Create a temporary 2Gb swap file so that we don't exaust the virtual
+# machines's memory when compiling scipy.
+if [ ! -f "/tmp/tmp_swap" ]; then
+    dd if=/dev/zero of=/tmp/tmp_swap bs=1024 count=2097152
+    mkswap /tmp/tmp_swap
+    swapon /tmp/tmp_swap
+fi
+
+# Install OpenCV
+pushd /tmp
+    git clone https://github.com/jayrambhia/Install-OpenCV.git
+    pushd Install-OpenCV/Ubuntu/
+        ./opencv_latest.sh
+    popd
+popd
+
+# Install required python packages.
+easy_install pip
+pip install -r /vagrant/requirements.txt
